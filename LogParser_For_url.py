649658864751cfb4e6256dd0a0997e2d028414c7
dogancankilment <dogancankilment@gmail.com>
2014-07-08 11:45:06 +0300

    selected block changed referer to url

--- LogParser_For_url.py
+++ LogParser_For_url.py
@@ -1,19 +1,20 @@
 from collections import Counter
 
-dosya=open("nginx.log")
+dosya=open("test.log")
 d={}
 i=0
 
 for satir in dosya.readlines():
     splitline = satir.split(' ') #bosluklara gore parcaliyor
-    d[i]=splitline[10] #http url log dosyasinin 10.kisimda yer aliyor
+    d[i]=splitline[6] #url log dosyasinda yer aliyor
     i=i+1
+
 cnt = Counter()
 
 for j in d.values():
     cnt[j]=cnt[j]+1
 
-#print cnt
+# print cnt
 print cnt.most_common(1)
 
 dosya.close()
