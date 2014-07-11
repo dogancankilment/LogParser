9b85b6a8733b5d72778d3318ad06abd1c37962fa
dogancankilment <dogancankilment@gmail.com>
2014-07-11 08:43:56 +0300

    function syntax added

--- LogParser_For_url.py
+++ LogParser_For_url.py
@@ -1,20 +1,24 @@
 from collections import Counter
 
-dosya=open("test.log")
-d={}
-i=0
+def log_parser():
+    dosya=open("test.log")
+    d={}
+    i=0
 
-for satir in dosya.readlines():
-    splitline = satir.split(' ') #bosluklara gore parcaliyor
-    d[i]=splitline[6] #url log dosyasinda yer aliyor
-    i=i+1
+    for satir in dosya.readlines():
+        splitline = satir.split(' ') #bosluklara gore parcaliyor
+        d[i]=splitline[6] #url log dosyasinda yer aliyor
+        i=i+1
 
-cnt = Counter()
+    cnt = Counter()
 
-for j in d.values():
-    cnt[j]=cnt[j]+1
+    for j in d.values():
+        cnt[j]=cnt[j]+1
 
-# print cnt
-print cnt.most_common(1)
+    # print cnt
+    print cnt.most_common(1)
 
-dosya.close()
+    dosya.close()
+
+if __name__ == '__main__':
+    log_parser()
